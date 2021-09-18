c5dc3002343151c2ed37e88d07d570484d1bc6d2
Andreas Wester <info@escapequest.se>
2021-09-18 12:09:32 +0200

    Solved 2015 day 1

--- 2015/day01/main.go
+++ 2015/day01/main.go
@@ -2,45 +2,41 @@ package main
 
 import (
 	"adventOfCode/util"
+	"errors"
 	"fmt"
 	"log"
 	"os"
-	"sort"
 )
 
-func part1(expenses []int, target int) int {
-	sort.Ints(expenses)
-	pointerLow, pointerHight := 0, len(expenses)-1
-	sum := expenses[pointerLow] + expenses[pointerHight]
-
-	for sum != target {
-		if sum > target {
-			pointerHight--
-		} else {
-			pointerLow++
+func part1(instructions string) int {
+	floor := 0
+	for _, char := range instructions {
+		if char == '(' {
+			floor++
+		} else if char == ')' {
+			floor--
 		}
-		if pointerHight == pointerLow {
-			return -1
-		}
-		sum = expenses[pointerLow] + expenses[pointerHight]
 	}
-
-	return expenses[pointerLow] * expenses[pointerHight]
+	return floor
 }
 
-func part2(expenses []int) int {
-	for pointer := 0; pointer < len(expenses)-2; pointer++ {
-		pointerValue := expenses[pointer]
-		partialResult := part1(expenses[pointer+1:len(expenses)-1], 2020-pointerValue)
-		if partialResult != -1 {
-			return partialResult * pointerValue
+func part2(instructions string) (int, error) {
+	floor := 0
+	for instruction, char := range instructions {
+		if char == '(' {
+			floor++
+		} else if char == ')' {
+			floor--
+		}
+		if floor == -1 {
+			return instruction + 1, nil
 		}
 	}
-	return -1
+	return -1, errors.New("never reached basement")
 }
 
 func main() {
-	file, err := os.Open("2020/day01/input.txt")
+	file, err := os.Open("2015/day01/input.txt")
 
 	if err != nil {
 		log.Fatal(err)
@@ -52,15 +48,18 @@ func main() {
 		}
 	}()
 
-	intArray, err := util.ReadIntsToArray(file)
+	stringSlice, err := util.ReadToString(file)
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	resultPt1 := part1(intArray, 2020)
-	resultPt2 := part2(intArray)
+	resultPt1 := part1(stringSlice)
+	resultPt2, err := part2(stringSlice)
 
 	fmt.Println(resultPt1)
+	if err != nil {
+		log.Fatal(err)
+	}
 	fmt.Println(resultPt2)
 }
--- 2015/day01/main_test.go
+++ 2015/day01/main_test.go
@@ -2,16 +2,109 @@ package main
 
 import "testing"
 
-func TestPart1(t *testing.T) {
-	actual := part1([]int{1721, 979, 366, 299, 675, 1456}, 2020)
-	if actual != 514579 {
-		t.Errorf("Expected %d to be 514579", actual)
+func Test_part1(t *testing.T) {
+	type args struct {
+		instructions string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "Test 1",
+			args: args{"(())"},
+			want: 0,
+		},
+		{
+			name: "Test 2",
+			args: args{"()()"},
+			want: 0,
+		},
+		{
+			name: "Test 3",
+			args: args{"((("},
+			want: 3,
+		},
+		{
+			name: "Test 4",
+			args: args{"(()(()("},
+			want: 3,
+		},
+		{
+			name: "Test 5",
+			args: args{"))((((("},
+			want: 3,
+		},
+		{
+			name: "Test 6",
+			args: args{"())"},
+			want: -1,
+		},
+		{
+			name: "Test 7",
+			args: args{"))("},
+			want: -1,
+		},
+		{
+			name: "Test 8",
+			args: args{")))"},
+			want: -3,
+		},
+		{
+			name: "Test 9",
+			args: args{")())())"},
+			want: -3,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := part1(tt.args.instructions); got != tt.want {
+				t.Errorf("part1() = %v, want %v", got, tt.want)
+			}
+		})
 	}
 }
 
-func TestPart2(t *testing.T) {
-	actual := part2([]int{1721, 979, 366, 299, 675, 1456})
-	if actual != 241861950 {
-		t.Errorf("Expected %d to be 241861950", actual)
+func Test_part2(t *testing.T) {
+	type args struct {
+		instructions string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    int
+		wantErr bool
+	}{
+		{
+			name:    "Test 1",
+			args:    args{")"},
+			want:    1,
+			wantErr: false,
+		},
+		{
+			name:    "Test 2",
+			args:    args{"()())"},
+			want:    5,
+			wantErr: false,
+		},
+		{
+			name:    "Test 3",
+			args:    args{"((((((("},
+			want:    -1,
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := part2(tt.args.instructions)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("part2() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("part2() got = %v, want %v", got, tt.want)
+			}
+		})
 	}
 }
